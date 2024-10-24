9de7c09c1e4c8aff93857e65297e4e0d122f7bc6
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-10-24 22:58:18 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="euclid_gcd_calculator",
+    version="0.1",
+    packages=find_packages(),
+    description="A Python package to calculate the GCD using Euclid's algorithm",
+    author="Sude",
+    license="MIT",
+    install_requires=[],
+)
